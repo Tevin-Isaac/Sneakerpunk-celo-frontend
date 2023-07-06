30ec304be81c1e5e743cdc6464bfcd4d9ce406cb
Tevin-Isaac <isaactevin@yahoo.com>
2023-07-06 20:10:36 +0300

    updated AboutUs.tsx file in components folder

--- packages/react-app/components/AboutUs.tsx
+++ packages/react-app/components/AboutUs.tsx
@@ -1,46 +1,41 @@
 import React from 'react';
-import {  Container } from '.';
-
+import { Container } from '.';
 
 import Image from 'next/image';
+
 const AboutUs = () => {
   return (
-    <Container className={'mb-44 scroll-mt-10'} id='about'>
-      <div className='text-center mb-8'>
+    <Container className={'mb-44 scroll-mt-10'}>
+      <div id='about' className='text-center mb-8'>
         <h2 className='font-bold text-3xl mb-2'>About Us</h2>
-     
-          <p className='opacity-50 w-full  sm:w-[400px] mx-auto'>
-            We love sneakers dont you?
-          </p>
-      
+
+        <p className='opacity-50 w-full  sm:w-[400px] mx-auto'>
+          We love sneakers do you?
+        </p>
       </div>
       <div className='flex items-center'>
         <div className='w-[44%] hidden sm:block'>
-         
-            <div className='relative w-full h-[700px] rounded-2xl overflow-hidden'>
-              <Image
-                layout='fill'
-                objectFit='cover'
-                objectPosition={'center'}
-                src='/aboutus.png'
-                alt='about us'
-              />
-            </div>
-          
+          <div className='relative w-full h-[700px] rounded-2xl overflow-hidden'>
+            <Image
+              layout='fill'
+              objectFit='cover'
+              objectPosition='center'
+              src='/aboutus.png'
+              alt='about us'
+            />
+          </div>
         </div>
         <div className='flex-1 ml-0 sm:ml-10 lg:ml-20'>
-          <h3 className='font-bold text-2xl mb-3'>Get Popular </h3>
+          <h3 className='font-bold text-2xl mb-3'>Get Popular</h3>
           <p className='mb-3 opacity-50'>
-            Sneakerpunk aims to make you favorite sneakers exist for life.Through this platform you can be able
-            to walk with confidence.{' '}
+            Sneakerpunk aims to make your favorite sneakers last for life. Through this platform, you can walk with confidence.
           </p>
           <p className='mb-3 opacity-50'>
-            Our team will help you in choosing the right sneakers{' '}
+            Our team will help you choose the right sneakers.
           </p>
           <p className='hidden lg:block mb-3 opacity-50'>
-            Our company started in 2019 and our main origin is inspired by Brandykicks where most of our sneakers come from.{' '}
+            Our company started in 2019, and our main inspiration comes from Brandykicks, where most of our sneakers come from.
           </p>
-         
         </div>
       </div>
     </Container>
